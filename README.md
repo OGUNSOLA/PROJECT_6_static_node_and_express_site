2f046d0335178a43d8e4fe38a5d439f7298adc3d
ogunsola <olubukola@me.com>
2021-03-07 00:11:15 -0500

    PROJECT 6 STATIC NODE.JS AND EXPRESS SITE
    TEAMTREEHOUSE.COM FULLSTACK JAVASCRIPT PROGRAM. PROJECT 6, STATIC NODE AND EXPRESS SITE

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # PROJECT_6_static_node_and_express_site
  Teamtreehouse.com fullstack JavaScript tech degree program. project 6 . Node and Express
+
+
+ css additions 
+ 
+/*  css addition */
+.error {
+  margin: 15px;
+  text-align: center;
+  color: red;
+}
+
+.error_img{
+  text-align: center;
+  /* margin-left: 10%; */
+}
+
+.celll{
+  transition: all 1s;
+}
+.cell:hover{
+  box-shadow: 2px 4px #35273d;
+  transform: skew(-1deg, -1deg);
+}
---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express(); // initialise the express app
+const {projects}= require('./data.json'); // import data 
+
+//app.use(bodyParser.urlencoded({extended: false}));
+app.use( express.static('public'));
+
+//set up view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+//Root route to home page
+app.get("/", (req, res)=>{
+  res.render('index', {projects:projects});
+});
+
+
+//route to About page
+app.get("/about", (req, res)=>{
+  res.render('about');
+});
+
+
+//route to Individual project page
+app.get("/projects/:id",(req, res, next)=>{
+  const projectId = req.params.id;
+  const project =projects.find(({id}) => id === +projectId);
+
+  // check to see if requested project is valid
+  if(project){
+        res.render('project', {project:project});
+  }else {
+    const err = new Error();
+    err.status = 404;
+    err.message = " oh oh!. Looks like the project you are looking for doesnt exist yet! "
+    next(err);
+  }
+});
+
+
+// 404 error handler
+app.use(( req, res, next)=>{
+
+  res.status(404).render('page-not-found');
+  console.log(err.message, err.status);
+
+});
+
+
+// global error handler
+app.use(( err, req, res, next) => {
+
+  if (err) {
+    console.log('Global error handler called', err);
+
+    if (err.status === 404) {
+      res.status(404).render('page-not-found', { err });
+      console.log(err.message, err.status);
+
+    } else {
+      err.message = err.message || `Bummer!  It looks like something went wrong on the server.`;
+      res.status(err.status || 500).render('error', { err });
+      console.log(err.message, err.status);
+  
+    }
+  }
+ 
+});
+
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log('Server listening on port 3000'));
